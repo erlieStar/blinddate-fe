bbec17c0ce57059c67d7eea609b273f2ddb93690
erlie <lilimin@social-touch.com>
2019-05-17 21:53:41 +0800

    项目初始化

---
+++ pages/ucenter/index/index.js
@@ -0,0 +1,63 @@
+var util = require('../../../utils/util.js');
+var api = require('../../../config/api.js');
+var user = require('../../../utils/user.js');
+var app = getApp();
+
+Page({
+  data: {
+    aboutShow: true,
+    userInfo: {
+      nickName: '点击登录',
+      avatarUrl: 'http://yanxuan.nosdn.127.net/8945ae63d940cc42406c3f67019c5cb6.png'
+    }
+  },
+  onLoad: function(options) {
+
+  },
+  onReady: function() {
+
+  },
+  onShow: function() {
+
+    //获取用户的登录信息
+    if (app.globalData.hasLogin) {
+      let userInfo = wx.getStorageSync('userInfo');
+      this.setData({
+        aboutShow: true,
+        userInfo: userInfo,
+      });
+    }
+
+  },
+  onHide: function() {
+    // 页面隐藏
+
+  },
+  onUnload: function() {
+    // 页面关闭
+  },
+  goLogin() {
+    if (!app.globalData.hasLogin) {
+      wx.navigateTo({
+        url: "/pages/auth/login/login"
+      });
+    }
+  },
+  exitLogin: function() {
+    wx.showModal({
+      title: '',
+      confirmColor: '#b4282d',
+      content: '退出登录？',
+      success: function(res) {
+        if (res.confirm) {
+          wx.removeStorageSync('token');
+          wx.removeStorageSync('userInfo');
+          wx.switchTab({
+            url: '/pages/index/index'
+          });
+        }
+      }
+    })
+
+  }
+})
